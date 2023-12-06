79eb0f402e82239a0a562138bc15dc8efc82e3de
Alina Laniuk <alinavoronina16@gmail.com>
2023-12-06 16:37:23 +0200

    fix: img style in CharInfo component

--- marvel-wiki/src/components/charInfo/CharInfo.js
+++ marvel-wiki/src/components/charInfo/CharInfo.js
@@ -65,6 +65,8 @@ class CharInfo extends Component {
     }
 }
 
+const imageNotFoundSrc = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
+
 const View = (props) => {
     const { id, name, description, thumbnail, homepage, wiki, comics } = props.char;
     const comicsList = [];
@@ -78,10 +80,11 @@ const View = (props) => {
     }
 
     const comicsContent = comicsList.length ? comicsList : noComicsMessage;
+    const imgStyle = thumbnail === imageNotFoundSrc ? {objectFit: 'contain'} : null;
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name} />
+                <img style={imgStyle} src={thumbnail} alt={name} />
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
