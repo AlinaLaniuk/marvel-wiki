93241a6930414eb8effc45ad7193550727e41c1b
Alina Laniuk <alinavoronina16@gmail.com>
2023-12-08 11:22:31 +0200

    feat: add types for props

--- marvel-wiki/src/components/charInfo/CharInfo.js
+++ marvel-wiki/src/components/charInfo/CharInfo.js
@@ -68,7 +68,7 @@ class CharInfo extends Component {
 const imageNotFoundSrc = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg';
 
 const View = (props) => {
-    const { id, name, description, thumbnail, homepage, wiki, comics } = props.char;
+    const { name, description, thumbnail, homepage, wiki, comics } = props.char;
     const comicsList = [];
     if (comics.length) {
         for (let i = 0; i < maxComicsOnPage; i++) {
